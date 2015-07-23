e029e945c021578b8d5cd92ba23b04042d6f8743
卢西 <lux@knownsec.com>
2015-07-23 20:23:28 +0800

    去除依赖grapnel

--- youdao-translate.el
+++ youdao-translate.el
@@ -7,10 +7,9 @@
 
 (require 'cl)
 (require 'json)
-(require 'grapnel)
 
 (defun youdao-translate-word ()
-  "查询被Mark的单词"
+  "查询被mark的单词"
   (interactive)
   (youdao-online-translate (buffer-substring-no-properties (mark) (point))))
 
@@ -21,29 +20,33 @@
 
 (defun show-basic-result (basic-data)
   (message
-   (with-output-to-string
-     (princ (format "英式发音：%s\n美式发音：%s\n"
-                    (cdr (assoc 'uk-phonetic basic-data))
-                    (cdr (assoc 'us-phonetic basic-data))))
-     (princ "基本释义：\n")
-     (loop for explain across (cdr (assoc 'explains basic-data))
-           do
-           (princ (format "%s\n" explain))))))
+   (if basic-data
+       (with-output-to-string
+         (princ (format "英式发音：%s\n美式发音：%s\n"
+                        (cdr (assoc 'uk-phonetic basic-data))
+                        (cdr (assoc 'us-phonetic basic-data))))
+         (princ "基本释义：\n")
+         (loop for explain across (cdr (assoc 'explains basic-data))
+               do
+               (princ (format "%s\n" explain))))
+     (princ "Not found"))))
+
+(defun url->content (url)
+  (with-current-buffer
+      (url-retrieve-synchronously url)
+    (goto-char (point-min))
+    (re-search-forward "^$")
+    (delete-region (point) (point-min))
+    (kill-line)
+    (buffer-string)))
 
 (defun youdao-online-translate (word)
-  (grapnel-retrieve-url
-   "http://fanyi.youdao.com/openapi.do"
-   '((success . (lambda (res hdrs)
-                  (let ((json-data (json-read-from-string res)))
-                    (show-basic-result (cdr (assoc 'basic json-data))))))
-     (failure . (lambda (res hdrs) (message "查询失败：%s" res)))
-     (error . (lambda (res err)  (message "查询错误：%s" err))))
-   "GET"
-   (list (cons 'keyfrom youdao-api-keyfrom)
-         (cons 'key youdao-api-key)
-         '(type . "data")
-         '(doctype . "json")
-         '(version . "1.1")
-         (cons 'q word))))
+  (let* ((api-url (format "http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&version=1.1&doctype=json&type=data&q=%s"
+                          youdao-api-keyfrom
+                          youdao-api-key
+                          word))
+         (url-data (decode-coding-string (url->content api-url) 'utf-8))
+         (json-data (json-read-from-string url-data)))
+    (show-basic-result (cdr (assoc 'basic json-data)))))
 
 (provide 'youdao-translate)
